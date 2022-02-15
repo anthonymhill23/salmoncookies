5f4da348dd3ceb2a324d0715017620367247ff5e
Anthony Hill <anthonymhill23@gmail.com>
2022-02-15 01:51:56 -0500

    refactored code, couldnt make it work properly

--- app.js
+++ app.js
@@ -1,31 +1,234 @@
-'use strict';
+'let seattle' = {
+  location: 'Seattle',
+  minCustHourly: 23,
+  maxCustHourly: 65,
+  aveCookiePer: 6.3,
+  custPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookies: 0,
+  getCustPerHour: function () {
+    for (let i = 0; i < storeHours.length; i++) {
+      this.custPerHour.push(randPerHour(this.minCustHourly, this.maxCustHourly));
+    }
+  },
+  getCookiesSoldPerHour: function () {
+    for (let j = 0; j < storeHours.length; j++) {
+      this.cookiesSoldPerHour.push(Math.floor(cookiesPerHour(this.custPerHour[j], this.aveCookiePer)));
+    }
+  },
+  getTotalCookies: function () { //This should total all the cookies sold.
+    for (let l = 0; l < storeHours.length; l++) {
+      this.totalCookies += this.cookiesSoldPerHour[l];
+    }
+  }
+};
 
-// --------------------------- global variables ---------------------------
+let tokyo = {
+  location: 'Tokyo',
+  minCustHourly: 3,
+  maxCustHourly: 24,
+  aveCookiePer: 1.2,
+  custPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookies: 0,
+  getCustPerHour: function () {
+    for (let i = 0; i < storeHours.length; i++) {
+      this.custPerHour.push(randPerHour(this.minCustHourly, this.maxCustHourly));
+    }
+  },
+  getCookiesSoldPerHour: function () {
+    for (let j = 0; j < storeHours.length; j++) {
+      this.cookiesSoldPerHour.push(Math.floor(cookiesPerHour(this.custPerHour[j], this.aveCookiePer)));
+    }
+  },
+  getTotalCookies: function () { //This should total all the cookies sold.
+    for (let l = 0; l < storeHours.length; l++) {
+      this.totalCookies += this.cookiesSoldPerHour[l];
+    }
+  }
+};
+
+let dubai = {
+  location: 'Dubai',
+  minCustHourly: 11,
+  maxCustHourly: 38,
+  aveCookiePer: 3.7,
+  custPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookies: 0,
+  getCustPerHour: function () {
+    for (let i = 0; i < storeHours.length; i++) {
+      this.custPerHour.push(randPerHour(this.minCustHourly, this.maxCustHourly));
+    }
+  },
+  getCookiesSoldPerHour: function () {
+    for (let j = 0; j < storeHours.length; j++) {
+      this.cookiesSoldPerHour.push(Math.floor(cookiesPerHour(this.custPerHour[j], this.aveCookiePer)));
+    }
+  },
+  getTotalCookies: function () { //This should total all the cookies sold.
+    for (let l = 0; l < storeHours.length; l++) {
+      this.totalCookies += this.cookiesSoldPerHour[l];
+    }
+  }
+};
+
+let paris = {
+  location: 'Paris',
+  minCustHourly: 20,
+  maxCustHourly: 38,
+  aveCookiePer: 2.3,
+  custPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookies: 0,
+  getCustPerHour: function () {
+    for (let i = 0; i < storeHours.length; i++) {
+      this.custPerHour.push(randPerHour(this.minCustHourly, this.maxCustHourly));
+    }
+  },
+  getCookiesSoldPerHour: function () {
+    for (let j = 0; j < storeHours.length; j++) {
+      this.cookiesSoldPerHour.push(Math.floor(cookiesPerHour(this.custPerHour[j], this.aveCookiePer)));
+    }
+  },
+  getTotalCookies: function () { //This should total all the cookies sold.
+    for (let l = 0; l < storeHours.length; l++) {
+      this.totalCookies += this.cookiesSoldPerHour[l];
+    }
+  }
+};
+
+let lima = {
+  location: 'Lima',
+  minCustHourly: 2,
+  maxCustHourly: 16,
+  aveCookiePer: 4.6,
+  custPerHour: [],
+  cookiesSoldPerHour: [],
+  totalCookies: 0,
+  getCustPerHour: function () {
+    for (let i = 0; i < storeHours.length; i++) {
+      this.custPerHour.push(randPerHour(this.minCustHourly, this.maxCustHourly));
+    }
+  },
+  getCookiesSoldPerHour: function () {
+    for (let j = 0; j < storeHours.length; j++) {
+      this.cookiesSoldPerHour.push(Math.floor(cookiesPerHour(this.custPerHour[j], this.aveCookiePer)));
+    }
+  },
+  getTotalCookies: function () { //This should total all the cookies sold.
+    for (let l = 0; l < storeHours.length; l++) {
+      this.totalCookies += this.cookiesSoldPerHour[l];
+    }
+  }
+};
+
+// DOM 
+// TO DO, Make render into a re-usable function so you can simply call it with the object name, to render the list in HTML
+
+seattle.render = function () {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = seattle.location;
+  cookieList.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  cookieList.appendChild(ulElem);
+  seattle.getCustPerHour();
+  seattle.getCookiesSoldPerHour();
+  seattle.getTotalCookies();
+  for (let i = 0; i < storeHours.length; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  let total = document.createElement('li');
+  total.textContent = `Total: ${this.totalCookies} cookies`;
+  ulElem.appendChild(total);
+};
+
+tokyo.render = function () {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = tokyo.location;
+  cookieList.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  cookieList.appendChild(ulElem);
+  tokyo.getCustPerHour();
+  tokyo.getCookiesSoldPerHour();
+  tokyo.getTotalCookies();
+  for (let i = 0; i < storeHours.length; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  let total = document.createElement('li');
+  total.textContent = `Total: ${this.totalCookies} cookies`;
+  ulElem.appendChild(total);
+};
+
+dubai.render = function () {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = dubai.location;
+  cookieList.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  cookieList.appendChild(ulElem);
+  dubai.getCustPerHour();
+  dubai.getCookiesSoldPerHour();
+  dubai.getTotalCookies();
+  for (let i = 0; i < storeHours.length; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  let total = document.createElement('li');
+  total.textContent = `Total: ${this.totalCookies} cookies`;
+  ulElem.appendChild(total);
+};
+
+paris.render = function () {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = paris.location;
+  cookieList.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  cookieList.appendChild(ulElem);
+  paris.getCustPerHour();
+  paris.getCookiesSoldPerHour();
+  paris.getTotalCookies();
+  for (let i = 0; i < storeHours.length; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  let total = document.createElement('li');
+  total.textContent = `Total: ${this.totalCookies} cookies`;
+  ulElem.appendChild(total);
+};
+
+lima.render = function () {
+  let h2Elem = document.createElement('h2');
+  h2Elem.textContent = lima.location;
+  cookieList.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  cookieList.appendChild(ulElem);
+  lima.getCustPerHour();
+  lima.getCookiesSoldPerHour();
+  lima.getTotalCookies();
+  for (let i = 0; i < storeHours.length; i++) {
+    let liElem = document.createElement('li');
+    liElem.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHour[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  let total = document.createElement('li');
+  total.textContent = `Total: ${this.totalCookies} cookies`;
+  ulElem.appendChild(total);
+};
 
 
-
-
-
-
-let cookieList = document.getElementById("cookieList");
-
-let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-function randPerHour(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function cookiesPerHour(custPerHour, aveCookiePer) {
-  let cookiesPerHour = custPerHour * aveCookiePer;
-  return cookiesPerHour;
-}
-
-
-
-
-
-
-
-
-
-
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
